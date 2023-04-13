f8a9ccdf3bfd191fe7cb22551dacda741a99f92a
Your Name <you@example>
2023-04-13 16:44:47 -0700

    Create homepage.

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,3 +1,24 @@
-export const Home = () =>{
-    return <div>Home </div>;
+import React, { useState } from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+
+export const Home = () => {
+  const navigate = useNavigate();
+  return (
+    <div class="text">
+      <h1>FITNESS CLUB.</h1>
+      <p>
+        If you want something you've never had, you must be willing to do
+        something you've never done
+      </p>
+      <button
+        onClick={() => {
+          navigate("/create-workout");
+        }}
+      >
+        Join Now
+      </button>
+    </div>
+  );
 };
