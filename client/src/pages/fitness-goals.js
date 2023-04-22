32169f1dd564bed0b2d8d9fb88e7b62f97aa2138
JerryF248 <61359857+JerryF248@users.noreply.github.com>
2023-04-22 14:51:46 -0700

    Fixed fitness goals page to utilize bootstrap

--- client/src/pages/fitness-goals.js
+++ client/src/pages/fitness-goals.js
@@ -11,39 +11,41 @@ export const FitnessGoals = () =>{
 
 const Create = () => {
     return (
-        <div class="row">
-        <div class="fitnessContainer">
-            <div class="fitnessCard" style={{ color: "gray" }}>
-                <img class = "img-responsive img-resize" src = {weightLossImg} alt="Gsirl with a tape measure around her waist, indicating weight loss."/>
+        <div class = "goalsContainer">
+            <div class="row">
+            <div class="fitnessContainer col-md-4">
+                <div class="fitnessCard" style={{ color: "gray" }}>
+                    <img class = "img-responsive img-resize" src = {weightLossImg} alt="Gsirl with a tape measure around her waist, indicating weight loss."/>
+                </div>
+                <h2>Fat Loss</h2>
+                <h2>"Weight Loss"</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                    eiusmod
+                </p>
             </div>
-            <h2>Fat Loss</h2>
-            <h2>"Weight Loss"</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod
-            </p>
-        </div>
-        <div class="fitnessContainer">
-            <div class="fitnessCard">
-                <img class = "img-responsive img-resize" src = {muscleImg} alt = "Muscular guy with his back turned, lifting dumbell weights."/>
+            <div class="fitnessContainer col-md-4">
+                <div class="fitnessCard">
+                    <img class = "img-responsive img-resize" src = {muscleImg} alt = "Muscular guy with his back turned, lifting dumbell weights."/>
+                </div>
+                <h2>Muscle Retention</h2>
+                <h2>"Bulking"</h2>
+                <p style={{ color: "red" }}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                    eiusmod
+                </p>
             </div>
-            <h2>Muscle Retention</h2>
-            <h2>"Bulking"</h2>
-            <p style={{ color: "red" }}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod
-            </p>
-        </div>
-        <div class="fitnessContainer">
-            <div class="fitnessCard">
-                <img class = "img-responsive img-resize" src = {cutImg} alt = "Muscular, lean boxer weighing in."/>
+            <div class="fitnessContainer col-md-4">
+                <div class="fitnessCard">
+                    <img class = "img-responsive img-resize" src = {cutImg} alt = "Muscular, lean boxer weighing in."/>
+                </div>
+                <h2>Fat Loss + Muscle Retention</h2>
+                <h2>"Cutting"</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                    eiusmod
+                </p>
             </div>
-            <h2>Fat Loss + Muscle Retention</h2>
-            <h2>"Cutting"</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod
-            </p>
         </div>
     </div>
     );
